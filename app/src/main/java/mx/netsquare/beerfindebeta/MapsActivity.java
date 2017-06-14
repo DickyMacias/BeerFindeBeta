1cdf5a28d73b6f05650da9b639fdd13aa432dc44
Dicky Macias <maciasfusco@gmail.com>
2017-06-14 11:21:12 -0600

    Ultima actualizacion de codigo

--- app/src/main/java/mx/netsquare/beerfindebeta/MapsActivity.java
+++ app/src/main/java/mx/netsquare/beerfindebeta/MapsActivity.java
@@ -3,11 +3,14 @@ package mx.netsquare.beerfindebeta;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -38,9 +41,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        //Delimitar el enfoque de la pantalla
+        LatLngBounds Chihuahua = new LatLngBounds(new LatLng(28.6, -106.1), new LatLng(28.7,-106.15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Chihuahua.getCenter(),12));
+
+        // Agregar sitios en el mapa
         LatLng utch = new LatLng(28.6458775, -106.1475035);
         mMap.addMarker(new MarkerOptions().position(utch).title("UTCH"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(utch));
+
+        LatLng expendio1 = new LatLng(28.6435345, -106.1163414);
+        mMap.addMarker(new MarkerOptions().position(expendio1).title("La casa"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(expendio1));
+
+        LatLng expendio2 = new LatLng(28.6641076, -106.1331413);
+        mMap.addMarker(new MarkerOptions().position(expendio2).title("La sacristia"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(expendio2));
+
+        LatLng expendio3 = new LatLng(28.6851961, -106.1336833);
+        mMap.addMarker(new MarkerOptions().position(expendio3).title("El panteon"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(expendio3));
+
+        //mMap.setMyLocationEnabled(true);
+
+
     }
 }
